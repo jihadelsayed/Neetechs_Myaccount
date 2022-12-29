c632824c773431d87c2f7ed0b46ec7f94a3ad371
jihad <david.pales1@gmail.com>
2022-12-29 20:01:28 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,38 @@
-import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'neetechs';
-  frameSrc
   LoginURL = environment.LoginURL;
-  ngOnInit() {
-    // Authorization
-    if (localStorage.getItem("userToken") == null) {
-      window.top.addEventListener("message", (event) => {
-        if (localStorage.getItem("userToken") == null) {
-          if (event.origin === "http://localhost:4200/" || event.origin === "https://neetechs.web.app/"|| event.origin === "https://neetechs.com/") {
-            return;
-          } else {
-            if (event.data['type'] === "credential") {
-              if (event.data.getToken !== 'undefined') {
-                localStorage.setItem("userToken", event.data.getToken)
-                localStorage.setItem("UserInfo", event.data.getUserInfo)
+
+  frameSrc
+  constructor(){}
+    ngOnInit() {
+      // Authorization
+      if (localStorage.getItem("userToken") == null) {
+        window.top.addEventListener("message", (event) => {
+          if (localStorage.getItem("userToken") == null) {
+            if (event.origin === "http://localhost:4200/" || event.origin === "https://neetechs.web.app/"|| event.origin === "https://neetechs.com/") {
+              return;
+            } else {
+              if (event.data['type'] === "credential") {
+                if (event.data.getToken !== 'undefined') {
+                  localStorage.setItem("userToken", event.data.getToken)
+                  localStorage.setItem("UserInfo", event.data.getUserInfo)
+                }
               }
             }
           }
-        }
-      }, false);
+        }, false);
+      }
+      this.frameSrc= this.LoginURL+ window.navigator.language.substring(0, 2)+"/#/getCredential"+"?"+ "host="+ window.location.href+"&"+"language="+ window.navigator.language +"&" + "pathname="+window.location.pathname;
+      document.getElementById('iframeAccount')["src"] = this.frameSrc;
+      console.log(this.frameSrc)
     }
-    this.frameSrc= this.LoginURL+ window.navigator.language.substring(0, 2)+"/#/getCredential"+"?"+ "host="+ window.location.href+"&"+"language="+ window.navigator.language +"&" + "pathname="+window.location.pathname;
-    document.getElementById('iframeAccount')["src"] = this.frameSrc;
-    console.log(this.frameSrc)
-  }
+
 }
