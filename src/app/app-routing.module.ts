6bc205e95dbb95ea69e248b046e77a401b81fea8
jihad <david.pales1@gmail.com>
2022-12-30 15:34:59 +0100

    sdfdsf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'src/services/auth/auth.guard';
 import { NotAuthGuard } from 'src/services/auth/not-auth.guard';
+import { ProfileComponent } from './Profile/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'Profile/:username',component: ProfileComponent,canActivate:[AuthGuard]},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy',useHash: true })],
